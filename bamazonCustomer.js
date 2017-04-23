5acab08edf0092ae40ed29d3a6989e861824f2d4
David Rivera <David@Davids-MacBook-Air.local>
2017-04-22 19:22:40 -0700

    BamazonApp

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,5 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-// var configs = require("./configs.js");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -14,8 +13,7 @@ var connection = mysql.createConnection({
 connection.connect(function(err)
 {
     if(err) throw err;
-    //console.log("connected as id " + connection.threadId);
-    //console.log(connection);
+
 });
 
 function start()
@@ -23,7 +21,7 @@ function start()
     connection.query("SELECT * FROM products", function(err, res)
     {
         if(err) throw err;
-        //console.log(JSON.stringify(res, null, 2));
+
         var items = [];
         res.forEach(function(value)
         {
@@ -32,11 +30,9 @@ function start()
             console.log("department: " + value.department_name);
             console.log("price: " + value.price);
             console.log("amount left: " + value.stock_quantity);
-            // console.log("total sales: " + value.product_sales);
             console.log("==========================================================\n")
             items.push(value.itemName);
         })
-        //console.log(items);
         buyItem();
     });
 }
@@ -74,10 +70,7 @@ function buyItem()
                     var query = "UPDATE products Set ? Where ?";
                     var new_stock = res[0].stock_quantity-quantity;
                     var new_sales = res[0].product_sales + quantity*res[0].price;
-                    //console.log("new stock is: " + new_stock);
-                    //console.log("total sales is: " + new_sales);
                     var params = [{stock_quantity: new_stock, product_sales: new_sales}, {item_id: itemID}];
-                    //console.log("SELECT * FROM departments WHERE department_name = '"+ res[0].department_name+"'");
                     connection.query("SELECT * FROM departments WHERE department_name = '"+ res[0].department_name+"'", function(err, data)
                     {
                         //console.log(data);
